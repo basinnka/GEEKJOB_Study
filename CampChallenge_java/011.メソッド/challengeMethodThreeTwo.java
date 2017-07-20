a64bec9f593d87bfb2b1e17ab067db52375ef59b
馬振華 <basinnka@yahoo.co.jp>
2017-07-20 14:52:09 +0900

    戻り値2課題を提出

--- CampChallenge_java/011.メソッド/challengeMethodThreeTwo.java
+++ CampChallenge_java/011.メソッド/challengeMethodThreeTwo.java
@@ -32,76 +32,66 @@ public class challengeMethodThreeTwo extends HttpServlet {
      *1. 戻り値として、人物のID、名前、生年月日、住所を配列にして返却するメソッドを作成してください。
      *2. 作成したメソッドを呼び出し、戻り値のID以外を表示してください。
      */
-    
-    //配列を作成
+    String[] prof(){
+        
+        String[] Profile1 ={"001","田中","1994-04-13","東京都江戸川区平井1-39-5"};
+        
+        return Profile1;
+    }
     /*
-    String Id1(String id1,String name1,String date1,String address1){
-        return "ID:"+id1+"名前:"+name1+"生年月日:"+date1+"住所:"+address1;   
-    }
-    String Id2(String id2,String name2,String date2,String address2){
-        return "ID:"+id2+"名前:"+name2+"生年月日:"+date2+"住所:"+address2;   
-    }
-    String Id3(String id3,String name3,String date3,String address3){
-        return "ID:"+id3+"名前:"+name3+"生年月日:"+date3+"住所:"+address3;   
-    }
-    */
+     *String Id1(String id1,String name1,String date1,String address1){
+     *   return "ID:"+id1+"名前:"+name1+"生年月日:"+date1+"住所:"+address1;   
+     *}
+     *String Id2(String id2,String name2,String date2,String address2){
+     *   return "ID:"+id2+"名前:"+name2+"生年月日:"+date2+"住所:"+address2;   
+     *}
+     *String Id3(String id3,String name3,String date3,String address3){
+     *   return "ID:"+id3+"名前:"+name3+"生年月日:"+date3+"住所:"+address3;   
+     *}
+     */
     
-    String [] Profile1 ={"ID1","NAME1","DATE1","ADDRESS1"};
-    int user1 (PrintWriter pw){
-        pw.print("ID:"+Profile1[0]+"<br>");
-        pw.print("名前:"+Profile1[1]+"<br>");
-        pw.print("生年月日:"+Profile1[2]+"<br>");
-        pw.print("住所:"+Profile1[3]+"<br>");
-        return 1;
-    }
-     String [] Profile2 ={"ID2","NAME2","DATE2","ADDRESS2"};
-    int user2 (PrintWriter pw){
-        pw.print("ID:"+Profile2[0]+"<br>");
-        pw.print("名前:"+Profile2[1]+"<br>");
-        pw.print("生年月日:"+Profile2[2]+"<br>");
-        pw.print("住所:"+Profile2[3]+"<br>");
-        return 2;
-    }
-     String [] Profile3 ={"ID3","NAME3","DATE3","ADDRESS3"};
-    int user3 (PrintWriter pw){
-        pw.println("ID:"+Profile3[0]+"<br>");
-        pw.println("名前:"+Profile3[1]+"<br>");
-        pw.println("生年月日:"+Profile3[2]+"<br>");
-        pw.println("住所:"+Profile3[3]+"<br>");
-        return 3;
-    }
+//    String [] Profile1 ={"ID1","NAME1","DATE1","ADDRESS1"};
+//    int user1 (PrintWriter pw){
+//        pw.print("ID:"+Profile1[0]+"<br>");
+//        pw.print("名前:"+Profile1[1]+"<br>");
+//        pw.print("生年月日:"+Profile1[2]+"<br>");
+//        pw.print("住所:"+Profile1[3]+"<br>");
+//        return 1;
+//    }
+//     String [] Profile2 ={"ID2","NAME2","DATE2","ADDRESS2"};
+//    int user2 (PrintWriter pw){
+//        pw.print("ID:"+Profile2[0]+"<br>");
+//        pw.print("名前:"+Profile2[1]+"<br>");
+//        pw.print("生年月日:"+Profile2[2]+"<br>");
+//        pw.print("住所:"+Profile2[3]+"<br>");
+//        return 2;
+//    }
+//     String [] Profile3 ={"ID3","NAME3","DATE3","ADDRESS3"};
+//    int user3 (PrintWriter pw){
+//        pw.println("ID:"+Profile3[0]+"<br>");
+//        pw.println("名前:"+Profile3[1]+"<br>");
+//        pw.println("生年月日:"+Profile3[2]+"<br>");
+//        pw.println("住所:"+Profile3[3]+"<br>");
+//        return 3;
+//    }    
     
-    
-        
-        
-   
-   
-   
-   
-   
-   
-    
+
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        int User=3;
-        switch (User){
-            case 1:
-                user1(out);
-                break;
-            case 2:
-                user2(out);
-                break;
-            case 3:
-                user3(out);
-                break;
+        
+            //作成したメソッドを呼び出し、戻り値として受け取った配列のID以外を表示してください。
+            //prof(); //{"001","田中","1994-04-13","東京都江戸川区平井1-39-5"};
+            out.println("名前:"+prof()[1]+"<br>"+"生年月日:"+prof()[2]+"<br>"+"住所:"+prof()[3]+"<br>");
+            
+
         }
         
         
            
-    }
+    
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
