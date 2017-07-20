6dd6cf20baa2ab7962b0a54b213b5353a94598e3
馬振華 <basinnka@yahoo.co.jp>
2017-07-20 12:24:04 +0900

    戻り値2課題を提出します

--- CampChallenge_java/011.メソッド/challengeMethodThreeTwo.java
+++ CampChallenge_java/011.メソッド/challengeMethodThreeTwo.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 
 /**
  *
@@ -32,29 +33,71 @@ public class challengeMethodThreeTwo extends HttpServlet {
      *2. 作成したメソッドを呼び出し、戻り値のID以外を表示してください。
      */
     
-    String PeopleId(String id){
-        return "人物のID:"+id;   
+    //配列を作成
+    /*
+    String Id1(String id1,String name1,String date1,String address1){
+        return "ID:"+id1+"名前:"+name1+"生年月日:"+date1+"住所:"+address1;   
     }
-    String PeopleName(String name){
-        return "名前:"+name;
+    String Id2(String id2,String name2,String date2,String address2){
+        return "ID:"+id2+"名前:"+name2+"生年月日:"+date2+"住所:"+address2;   
     }
-    String PeopleDate(String date){
-        return "生年月日:"+date;
+    String Id3(String id3,String name3,String date3,String address3){
+        return "ID:"+id3+"名前:"+name3+"生年月日:"+date3+"住所:"+address3;   
     }
-    String PeopleAddress(String address){
-        return "住所:"+address;
+    */
+    
+    String [] Profile1 ={"ID1","NAME1","DATE1","ADDRESS1"};
+    int user1 (PrintWriter pw){
+        pw.print("ID:"+Profile1[0]+"<br>");
+        pw.print("名前:"+Profile1[1]+"<br>");
+        pw.print("生年月日:"+Profile1[2]+"<br>");
+        pw.print("住所:"+Profile1[3]+"<br>");
+        return 1;
+    }
+     String [] Profile2 ={"ID2","NAME2","DATE2","ADDRESS2"};
+    int user2 (PrintWriter pw){
+        pw.print("ID:"+Profile2[0]+"<br>");
+        pw.print("名前:"+Profile2[1]+"<br>");
+        pw.print("生年月日:"+Profile2[2]+"<br>");
+        pw.print("住所:"+Profile2[3]+"<br>");
+        return 2;
+    }
+     String [] Profile3 ={"ID3","NAME3","DATE3","ADDRESS3"};
+    int user3 (PrintWriter pw){
+        pw.println("ID:"+Profile3[0]+"<br>");
+        pw.println("名前:"+Profile3[1]+"<br>");
+        pw.println("生年月日:"+Profile3[2]+"<br>");
+        pw.println("住所:"+Profile3[3]+"<br>");
+        return 3;
     }
     
+    
+        
+        
+   
+   
+   
+   
+   
+   
     
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        out.println(PeopleId(" 0123456789<br>"));
-        out.println(PeopleName(" 田中<br>"));
-        out.println(PeopleDate(" 1999/07/18<br>"));
-        out.println(PeopleAddress(" 東京都江戸川区平井1-13-5<br>"));
+        int User=3;
+        switch (User){
+            case 1:
+                user1(out);
+                break;
+            case 2:
+                user2(out);
+                break;
+            case 3:
+                user3(out);
+                break;
+        }
         
         
            
