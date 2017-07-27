8c11eb4d0e6015b6f59bd1f02fab7cd175206c2e
馬振華 <basinnka@yahoo.co.jp>
2017-07-27 12:57:00 +0900

    タイムスタンプの表示課題1を提出する

---
+++ CampChallenge_java/015.標準クラス/Challenge01503.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Challenge015;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+
+
+/**
+ * タイムスタンプの表示
+ * @author MAZHENHUA
+ * 課題：2016年11月4日 10時0分0秒のタイムスタンプを作成し、「年-月-日 時:分:秒」で表示してください。
+ */
+public class Challenge01503 {
+    public static void main(String []args){
+        //Calendar c =Calendar.getInstance();
+        //c.set(2016, 11,4,10,0,0);
+        Date c = new Date("2016/11/04 10:00:00");
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.print(sdf.format(c));
+    }
+    
+}
