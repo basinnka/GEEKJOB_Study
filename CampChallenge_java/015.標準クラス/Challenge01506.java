29b8c89a24f07eb46a749307a07a3c257e7b0548
馬振華 <basinnka@yahoo.co.jp>
2017-07-27 16:12:29 +0900

    文字数の取得課題を提出します

---
+++ CampChallenge_java/015.標準クラス/Challenge01506.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Challenge015;
+
+/**
+ * 文字数の取得
+ * @author MAZHENHUA
+ * 自分のメールアドレスの「@」以降の文字を取得して、表示してください。
+ */
+public class Challenge01506 {
+    public static void main(String []args){
+        String m ="basinnka@yahoo.co.jp";
+        System.out.println(m.substring(9));
+    }
+    
+}
